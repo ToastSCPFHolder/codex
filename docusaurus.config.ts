64c139dcbb61c773a15aa88451e64164a5b9bec2
StayBlue <blue@spook.rip>
2025-08-25 19:42:25 -0700

    update URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,14 +8,12 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://toastmage-scpf.github.io",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/codex",
+  url: "https://codex.areaomega.com",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "toastmage-scpf", // Usually your GitHub org/user name.
+  organizationName: "ToastSCPFHolder", // Usually your GitHub org/user name.
   projectName: "codex", // Usually your repo name.
 
   onBrokenLinks: "throw",
